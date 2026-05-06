f971d966a299aefe2aef4db7e59ee74e2ad2d7a8
batteryspecial <infinityscrub74@gmail.com>
2026-05-06 12:03:51 -0600

    fix: removed secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z(wf5g#=jxwl#-1(as!%vh*483^qp$p7+zjo2-#kw_+p03)=uk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
